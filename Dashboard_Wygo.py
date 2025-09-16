86c32032fd764802f60c456dc40df19655ecc482
Raphael Schüpbach <raphael.schuepbach@students.fhnw.ch>
2025-09-16 14:07:13 +0200

    Update Dashboard_Wygo.py

--- Dashboard_Wygo.py
+++ Dashboard_Wygo.py
@@ -24,7 +24,7 @@ bully_stats = bully_stats.reset_index()
 
 # Funktion für Top-3 Balkendiagramm (mit dtick=1)
 def plot_top3(df, column, title, color):
-    top3 = df.groupby("Name")[column].sum().sort_values(ascending=False).head(3).reset_index()
+    top3 = df.groupby("Name")[column].sum().sort_values(ascending=False).head(13).reset_index()
     fig = px.bar(top3, x="Name", y=column, title=title, text=column)
     fig.update_traces(marker_color=color, texttemplate='%{text}', textposition='inside',
                       hoverinfo='skip', hovertemplate=None, showlegend=False)
@@ -35,8 +35,8 @@ def plot_top3(df, column, title, color):
 
 # Spezielle Funktion für Bully-Gewinn % (ohne dtick=1)
 def plot_top3_bully(bully_df):
-    top3 = bully_df.sort_values("Bully-Gewinn %", ascending=False).head(3)
-    fig = px.bar(top3, x="Name", y="Bully-Gewinn %", title="Top 3 Bully-Gewinnquote", text="Bully-Gewinn %")
+    top3 = bully_df.sort_values("Bully-Gewinn %", ascending=False).head(13)
+    fig = px.bar(top3, x="Name", y="Bully-Gewinn %", title="Bully-Gewinnquote", text="Bully-Gewinn %")
     fig.update_traces(marker_color="purple", texttemplate='%{text:.1f}%', textposition='inside',
                       hoverinfo='skip', hovertemplate=None, showlegend=False)
     fig.update_layout(showlegend=False, yaxis_title=None, xaxis_title=None)
@@ -48,9 +48,9 @@ st.set_page_config(page_title="Spielerstatistik UHC Wygorazzi", layout="centered
 st.title("Spielerstatistik UHC Wygorazzi")
 st.caption("Saison 25/26 – Spielerstatistiken")
 
-st.plotly_chart(plot_top3(df, "T", "Top 3 Tore", "royalblue"), use_container_width=True, config={'staticPlot': True})
-st.plotly_chart(plot_top3(df, "A", "Top 3 Assists", "seagreen"), use_container_width=True, config={'staticPlot': True})
-st.plotly_chart(plot_top3(df, "Punkte", "Top 3 Punkte (T+A)", "darkorange"), use_container_width=True, config={'staticPlot': True})
-st.plotly_chart(plot_top3(df, "PlusMinus", "Top 3 Plus-Minus", "firebrick"), use_container_width=True, config={'staticPlot': True})
+st.plotly_chart(plot_top3(df, "T", "Tore", "royalblue"), use_container_width=True, config={'staticPlot': True})
+st.plotly_chart(plot_top3(df, "A", "Assists", "seagreen"), use_container_width=True, config={'staticPlot': True})
+st.plotly_chart(plot_top3(df, "Punkte", "Punkte (T+A)", "darkorange"), use_container_width=True, config={'staticPlot': True})
+st.plotly_chart(plot_top3(df, "PlusMinus", "Plus-Minus", "firebrick"), use_container_width=True, config={'staticPlot': True})
 st.plotly_chart(plot_top3_bully(bully_stats), use_container_width=True, config={'staticPlot': True})
 
